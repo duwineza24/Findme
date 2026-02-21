1d1cc3557d224193267dfddc5c99634cb0d67875
Devothe Uwineza <devothauwineza7@gmail.com>
2026-02-21 15:40:56 +0200

    update main and vite

--- FRONTEND/findme/src/main.jsx
+++ FRONTEND/findme/src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter >
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
