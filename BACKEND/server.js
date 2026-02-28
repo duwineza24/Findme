fa50d4c25dfcc2b7aa8fe8d9c106736d00f1ecfc
Devothe Uwineza <devothauwineza7@gmail.com>
2026-02-28 11:50:10 +0200

    update backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -9,12 +9,7 @@ dotenv.config();
 const app = express();
 
 // CORS setup
-app.use(cors({
-  origin: 'https://findme-opal.vercel.app/',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 // Handle preflight requests
 app.options('/{*path}', cors());
