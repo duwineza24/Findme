a683afb00557c3eaa39c696bfce81dc446e7bf19
Devothe Uwineza <devothauwineza7@gmail.com>
2026-02-28 10:30:17 +0200

    update backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const connectDB = require("./config/db");
+const cors = require("cors");
+
 
 dotenv.config();
-connectDB();
 
 const app = express();
 const path = require("path");
-
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(cors());
+
+connectDB();
+
+
+
+
 
 
 // middleware
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 const userRoute=require("./routes/userRoute")
 const itemRoute=require("./routes/itemRoute")
 const matchRoutes = require("./routes/matchRoute");
@@ -30,9 +38,15 @@ app.use("/api/match", require("./routes/matchRoute"));
 app.use("/api/chat", require("./routes/chatRoutes"));
 app.use("/api/notification", require("./routes/notificationRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
+
+
 app.get("/", (req, res) => {
   res.json({ message: "API is running" });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, (err) => {
+  if (err) console.error(err);
+  else console.log(`Server running on port ${PORT}`);
+});
